f287a21c5320f47226b3b6e4cfbcebaded0ea571
nongdingan <nda_1992@163.com>
2021-11-04 21:25:01 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<!-- 首页 -->
+	<view class="index-container">
+		<view class="index-swiper">
+			123
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+						text: '点赞',
+						color: 'blue',
+						fontSize: 28
+						}, {
+							text: '分享'
+						}, {
+							text: '评论'
+						}],
+				show: true
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.index-container {
+		.index-swiper {
+			color: $u-type-primary;
+		}
+	}
+</style>
